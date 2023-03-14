1b6da9f2db9240ed18796130f610b43fcd963341
Muhammad Rival <em.rival282@gmail.com>
2023-03-14 15:36:48 +0700

    (app): add migration
    add new table migration at post table
    
    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,6 +16,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = DB::table('posts')
+            ->where('active', true)
             ->get();
 
         $data = [
