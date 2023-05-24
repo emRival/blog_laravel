1722e79b87b67d717b4291849eb75ed8fc040773
Muhammad Rival <em.rival282@gmail.com>
2023-05-24 13:49:56 +0700

    delete verification email

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-})->middleware(['auth', 'verified']);
+})->middleware(['auth']);
 //AUTH
 // Route::get('login', [AuthController::class, 'login'])->name('login');
 // Route::post('login', [AuthController::class, 'authenticate']);
