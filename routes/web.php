c82bb2f9eed80c607ae81dc42c5ee20eab026d7b
rodhisz <rodhisz.45@gmail.com>
2023-05-24 14:32:33 +0700

    add landingcontroller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,9 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [LandingController::class, 'index']);
+
 //AUTH
 // Route::get('login', [AuthController::class, 'login'])->name('login');
 // Route::post('login', [AuthController::class, 'authenticate']);
