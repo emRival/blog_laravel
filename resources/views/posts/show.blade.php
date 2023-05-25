d5ad69dc66ab16bce77a0308628d6b30f9e0f471
Muhammad Rival <em.rival282@gmail.com>
2023-05-25 07:46:49 +0700

    count comment to 3

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
     <article class="blog-post mt-5">
-        <img src="{{ url('storage/' . $post->image) }}" class="card-img-top object-fit-cover"
-        width="100%" height="350" alt="photo">
+        <img src="{{ url('storage/' . $post->image) }}" class="card-img-top object-fit-cover" width="100%" height="350"
+            alt="photo">
         <h2 class="blog-post-title mb-1">{{ $post->title }}</h2>
         <p class="blog-post-meta"><small class="text-muted">Created By {{ $post->postwriter->name }} At
                 {{ date('d M Y H:i', strtotime($post->created_at)) }}
@@ -14,40 +14,78 @@
     </article>
 
     <p class="text-muted">{{ $total_comments }} Komentar</p>
-    @foreach ($comments as $comment)
 
-    <div class="card mb-3">
-        <div class="card-header text-primary">
-            &#64;{{ $comment->commentwriter->name }}
+    @foreach ($comments->take(3) as $comment)
+        <div class="card mb-3">
+            <div class="card-header text-primary">
+                &#64;{{ $comment->commentwriter->name }}
+            </div>
+            <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                    <p>{{ $comment->comment }}</p>
+                    <p class="blockquote-footer"><small>Commented At :
+                            {{ date('d M Y H:i', strtotime($comment->created_at)) }}</small></p>
+                </blockquote>
+            </div>
         </div>
-        <div class="card-body">
-          <blockquote class="blockquote mb-0">
-            <p>{{ $comment->comment }}</p>
-            <p class="blockquote-footer"><small>Commented At : {{ date('d M Y H:i', strtotime($comment->created_at)) }}</small></p>
-          </blockquote>
-        </div>
-      </div>
-
-
     @endforeach
 
+    @if ($comments->count() > 3)
 
-    @if (Auth::check())
-    <form class="d-flex mb-4" action="{{ route('comment') }}" method="post">
-        @csrf
-        <div class="input-group mb-3">
-            <input type="text" class="form-control form-control-color-danger" name="comment"
-                placeholder="Recipient's username">
-            <input type="hidden" name="post_id" value="{{ $post->id }}">
-            <button class="btn btn-outline-secondary" type="submit"><i class="fa-solid fa-paper-plane"></i></button>
+    <div class="position-relative">
+        <a class="position-absolute top-50 start-50 translate-middle" id="showAllComments">Show All Comments</a>
+    </div>
+
+        <div id="hiddenComments" style="display: none;">
+            @foreach ($comments->skip(3) as $comment)
+                <div class="card mb-3">
+                    <div class="card-header text-primary">
+                        &#64;{{ $comment->commentwriter->name }}
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>{{ $comment->comment }}</p>
+                            <p class="blockquote-footer"><small>Commented At :
+                                    {{ date('d M Y H:i', strtotime($comment->created_at)) }}</small></p>
+                        </blockquote>
+                    </div>
+                </div>
+            @endforeach
         </div>
 
-    </form>
+
+        <script>
+            document.getElementById("showAllComments").addEventListener("click", function() {
+                document.getElementById("hiddenComments").style.display = "block";
+                this.style.display = "none";
+            });
+        </script>
     @endif
 
 
 
+    @if (Auth::check())
+        <form class="d-flex my-5" action="{{ route('comment') }}" method="post">
+            @csrf
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" name="comment"
+                    placeholder="Recipient's username">
+                <input type="hidden" name="post_id" value="{{ $post->id }}">
+                <button class="btn btn-outline-secondary" type="submit"><i class="fa-solid fa-paper-plane"></i></button>
+            </div>
+
+        </form>
+    @endif
+
+
+    @if (Auth::check())
     <a href="{{ url('posts') }}" class="btn btn-success">Kembali</a>
+    @else
+    <a href="{{ url('/') }}" class="btn btn-success">Kembali</a>
+    @endif
+
+
+
 
 
 @endsection
